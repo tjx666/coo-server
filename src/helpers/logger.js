d5cf1e6ec8c8f5e8a5b72eaac1a7426caa28db09
YuTengjing <ytj2713151713@gmail.com>
2019-12-17 23:54:41 +0800

    :loud_sound: helpers: integrate log4js as logger

---
+++ src/helpers/logger.js
@@ -0,0 +1,73 @@
+const { resolve } = require('path');
+const log4js = require('log4js');
+const fs = require('fs-extra');
+
+const projectRoot = resolve(__dirname, '../../');
+const appLogPath = resolve(projectRoot, './logs/application.log');
+const ctxLogPath = resolve(projectRoot, './logs/context.log');
+const appErrorLogPath = resolve(projectRoot, './logs/application.error.log');
+const ctxErrorLogPath = resolve(projectRoot, './logs/context.error.log');
+
+log4js.configure({
+    appenders: {
+        appFile: {
+            type: 'dateFile',
+            filename: appLogPath,
+        },
+        ctxFile: {
+            type: 'dateFile',
+            filename: ctxLogPath,
+        },
+        appErrorFile: {
+            type: 'logLevelFilter',
+            appender: 'appFile',
+            filename: appErrorLogPath,
+            level: 'error',
+        },
+        ctxErrorFile: {
+            type: 'logLevelFilter',
+            appender: 'ctxErrorDateFile',
+            filename: ctxErrorLogPath,
+            level: 'error',
+        },
+        console: {
+            type: 'console',
+            layout: {
+                type: 'pattern',
+                pattern: '%[[%p] %c%] - %m',
+            },
+        },
+    },
+    categories: {
+        default: {
+            appenders: ['console'],
+            level: 'trace',
+        },
+        application: {
+            appenders: ['console', 'appFile', 'appErrorFile'],
+            level: 'trace',
+        },
+        context: {
+            appenders: ['console', 'ctxFile', 'ctxErrorFile'],
+            level: 'trace',
+        },
+    },
+});
+
+const clearLog = async () => {
+    await fs.remove(projectRoot, './log');
+};
+
+const logHelpers = {
+    appLogger: log4js.getLogger('application'),
+    ctxLogger: log4js.getLogger('context'),
+    logger: log4js.getLogger('console'),
+    clearLog,
+};
+
+const loggerHelper = server => {
+    Object.assign(server, logHelpers);
+    Object.assign(server.context, logHelpers);
+};
+
+module.exports = loggerHelper;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const requestLogger = require('koa-logger');
 const chalk = require('chalk');
+const loggerHelpers = require('./helpers/logger');
 const serverConfig = require('../configs/server');
 
 const server = new Koa();
 
+loggerHelpers(server);
+const { appLogger } = server;
+const mode = process.env.NODE_ENV || 'production';
+appLogger.info(`Startup server under ${chalk.bold.yellow(mode)} mode`);
+
+server.use(requestLogger());
 server.use(bodyParser());
 
 const { HOST, PORT } = serverConfig;
 server.listen(PORT, HOST, () => {
     const serverAddr = `http://${HOST}:${PORT}`;
-    console.log(`Server is running at ${chalk.green.underline(serverAddr)}`);
+    appLogger.info(`Server is running at ${chalk.green.underline(serverAddr)}`);
 });
